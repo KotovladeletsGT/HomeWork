c8c4aef4239670840cc452bf452aedf4c0b7f852
KotovladeletsGT <72261536+KotovladeletsGT@users.noreply.github.com>
2024-04-27 15:44:38 +0300

    Add files via upload

---
+++ A14 Max of 3 digits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+int a, b, c, d;
+
+    scanf ("%d", &a);
+    b = a%10;
+    c = (a/10)%10;
+    d = (a/100)%10;
+    
+    
+    b = (c>b) ? c : b;
+    b = (d>b) ? d : b;
+   
+    printf("%d", b);
+    return 0;
+}
