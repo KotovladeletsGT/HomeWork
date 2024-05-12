1d737e804667250777a3a51d04294e0c0204a6be
KotovladeletsGT <72261536+KotovladeletsGT@users.noreply.github.com>
2024-05-12 20:54:55 +0300

    Add files via upload

---
+++ C_tasks/C3_middle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b, res;
+    scanf("%d%d",&a,&b);
+    res = middle(a,b);
+    printf("%d",res);
+    return 0;
+}
+
+
+int middle(a,b) {
+    int res;
+    res=a+b;
+    res=res/2;
+
+    return res;
+}
+
+
